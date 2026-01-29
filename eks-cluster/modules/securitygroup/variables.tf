8bdd33565e60a25b200e3d631c181ced20426a86
afiq <afiqezar96@gmail.com>
2026-01-29 07:46:29 +0000

    cleanup

--- eks-cluster/modules/securitygroup/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "environment" {
-  description = "Environment name (dev, staging, uat, prod)."
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "VPC ID where security groups will be created."
-  type        = string
-}
